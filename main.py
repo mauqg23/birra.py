ce571101ca0151d7f1cc284112cd279371427a11
Mauricio <mauqg23@gmail.com>
2025-11-22 08:20:05 -0600

    primer commit del proyecto, solo empece y puse los tags del objeto birra

---
+++ main.py
@@ -0,0 +1,11 @@
+#nombre, color, notas, ph, abv, ibu, stock, ratings
+
+class birra:
+    def __init__(self, color, notas, ph, abv, ibu, stock, ratings):
+        self.color = color
+        self.notas = notas
+        self.ph = ph
+        self.abv = abv
+        self.ibu = ibu
+        self.stock = stock
+        self.ratings = ratings
