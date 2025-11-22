e6f3a7219dba6b329288d131c0e35b7f022ee9e3
Mauricio <mauqg23@gmail.com>
2025-11-22 09:36:32 -0600

    agregue dos metodos, pedir y catalogo

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-#nombre, color, notas, ph, abv, ibu, stock, ratings
-
 class birra:
-    def __init__(self, color, notas, ph, abv, ibu, stock, ratings):
+    def __init__(self, name, color, notas, ph, abv, ibu, stock, ratings):
         self.color = color
         self.notas = notas
         self.ph = ph
@@ -9,3 +7,16 @@ class birra:
         self.ibu = ibu
         self.stock = stock
         self.ratings = ratings
+        self.name = name
+
+    def pedir(self):
+        if self.stock > 0:
+            self.stock -=1
+            print(f"Usted ha consumido una {self.name}")
+        else:
+            print(f"Lo sentimos pero no hay {self.name}")
+
+    def catalogo(self, lista_cervezas):
+        for cerveza in lista_cervezas:
+            print(f'{cerveza.name} | Color: {cerveza.color}, Notas: {cerveza.notas}, PH: {cerveza.ph}, abv:  {cerveza.abv}, Stock: {cerveza.stock} y Rating: {cerveza.ratings}')
+        
